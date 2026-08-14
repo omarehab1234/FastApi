7a9905974742078d494b1c377451b75ca858871e
omarehab1234 <omarehab06@gmail.com>
2026-08-14 20:17:43 +0300

    increase capacity off user password

--- modelDB/User.py
+++ modelDB/User.py
@@ -10,7 +10,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    email = Column(String(100), unique=True, index=True)
-    password = Column(String(25))
-    role = Column(String(20), default="user")
+    username = Column(String(100), unique=True, index=True)
+    email = Column(String(255), unique=True, index=True)
+    password = Column(String(255), nullable=False)
+    role = Column(String(50), default="user")
